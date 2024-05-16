203012dec9d6452821b4c485ff055e756d2c1457
Lianny Coringa <lianny_costa@hotmail.com>
2024-05-16 00:28:42 -0300

    Fazendo os testes cypress

---
+++ cypress/e2e/Login.cy.ts
@@ -0,0 +1,18 @@
+/// <reference types="cypress" />
+
+describe('template spec', () => {
+  it('Testa os textos da página de login', () => {
+    cy.visit('http://localhost:5173/')
+    cy.contains('b2b').should('be.visible')
+    cy.contains('E-mail').should('be.visible')
+    cy.contains('Password').should('be.visible')
+    cy.contains('Sing In').should('be.visible')
+  })
+  it('Testa se o usuário é redirecionado para a página de Profile', () => {
+    cy.visit('http://localhost:5173/')
+    cy.get('input[placeholder="@gmail.com"]').type('cliente@youdrive.com')
+    cy.get('input[placeholder="***************"]').type('password')
+    cy.get('button').click()
+    cy.contains('Profile picture').should('be.visible')
+  })
+})
